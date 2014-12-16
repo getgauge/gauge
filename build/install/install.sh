d6f6edee38fa9c2da4937127a496a161d3da98c8
prateekbaheti <prateektheone@gmail.com>
2014-12-16 11:26:49 +0530

    Modifying install shell script to install as sudo only when default prefix used

--- build/install/install.sh
+++ build/install/install.sh
@@ -2,12 +2,17 @@
 if [ -z "$prefix" ];
 then
 	prefix=/usr/local
+  echo "creating $prefix if it doesn't exist"
+  [ -d $prefix ] || mkdir $prefix
+  echo "Copying files to $prefix"
+  sudo /bin/sh -c "cp -rf bin share $prefix"
+else
+  echo "creating $prefix if it doesn't exist"
+  [ -d $prefix ] || mkdir $prefix
+  echo "Copying files to $prefix"
+  cp -rf bin share $prefix
 fi
 
-echo "creating $prefix if it doesn't exist"
-[ -d $prefix ] || mkdir $prefix
-echo "Copying files to $prefix"
-cp -rf bin share $prefix
 echo "Done installing Gauge to $prefix"
 echo "Installing plugin - html-report"
 gauge --install html-report
