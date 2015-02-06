db23038914b12f81b4e7bda8c6a10c3c1bbf2706
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-02-06 15:22:47 +0530

    added copyright license header to all files

--- genproto.sh
+++ genproto.sh
@@ -1,3 +1,20 @@
+# Copyright 2014 ThoughtWorks, Inc.
+
+# This file is part of Gauge.
+
+# Gauge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Gauge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/bin/sh
 
 #Using protoc version 2.5.0
@@ -8,6 +25,6 @@ PATH=$PATH:$GOPATH/bin protoc --go_out=../gauge_messages messages.proto
 PATH=$PATH:$GOPATH/bin protoc --go_out=../gauge_messages api.proto
 
 cd ..
-sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/messages.pb.go && sed  -i.backup 's/gauge_messages1.//g' gauge_messages/messages.pb.go && rm gauge_messages/messages.pb.go.backup
-sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/api.pb.go && sed  -i.backup 's/gauge_messages1.//g' gauge_messages/api.pb.go && rm gauge_messages/api.pb.go.backup
+sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/messages.pb.go && sed  -i.backup 's/gauge_messages1.#g' gauge_messages/messages.pb.go && rm gauge_messages/messages.pb.go.backup
+sed  -i.backup '/import gauge_messages1 "spec.pb"/d' gauge_messages/api.pb.go && sed  -i.backup 's/gauge_messages1.#g' gauge_messages/api.pb.go && rm gauge_messages/api.pb.go.backup
 go fmt github.com/getgauge/gauge/...
