84156b00716c0e6716638fdb592a17a7afc06c97
Martin Feckie <mfeckie@gmail.com>
2015-04-13 22:33:09 +0800

    Refactor version.go and bugfix
    Create an versionError string to reduce verbosity
    
    Prior to this fix version was reporting incorrect errors due to wrong
    indexes.

--- version_test.go
+++ version_test.go
@@ -31,26 +31,21 @@ func (s *MySuite) TestParsingVersion(c *C) {
 
 func (s *MySuite) TestParsingErrorForIncorrectNumberOfDotCharacters(c *C) {
 	_, err := parseVersion("1.5.9.9")
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "Incorrect number of '.' characters in Version. Version should be of the form 1.5.7")
+	c.Assert(err, ErrorMatches, "Incorrect version format. Version should be in the form 1.5.7")
 
 	_, err = parseVersion("0.")
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "Incorrect number of '.' characters in Version. Version should be of the form 1.5.7")
+	c.Assert(err, ErrorMatches, "Incorrect version format. Version should be in the form 1.5.7")
 }
 
 func (s *MySuite) TestParsingErrorForNonIntegerVersion(c *C) {
 	_, err := parseVersion("a.9.0")
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "Error parsing major version number a to integer. strconv.ParseInt: parsing \"a\": invalid syntax")
+	c.Assert(err, ErrorMatches, `Error parsing major version a to integer. strconv.ParseInt: parsing "a": invalid syntax`)
 
 	_, err = parseVersion("0.ffhj.78")
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "Error parsing minor version number 0 to integer. strconv.ParseInt: parsing \"ffhj\": invalid syntax")
+	c.Assert(err, ErrorMatches, `Error parsing minor version ffhj to integer. strconv.ParseInt: parsing "ffhj": invalid syntax`)
 
 	_, err = parseVersion("8.9.opl")
-	c.Assert(err, NotNil)
-	c.Assert(err.Error(), Equals, "Error parsing patch version number 8 to integer. strconv.ParseInt: parsing \"opl\": invalid syntax")
+	c.Assert(err, ErrorMatches, `Error parsing patch version opl to integer. strconv.ParseInt: parsing "opl": invalid syntax`)
 }
 
 func (s *MySuite) TestVersionComparisonGreaterLesser(c *C) {
