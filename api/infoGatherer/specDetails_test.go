396a591b496f7361fce7ac576a70c55edf57a453
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-09-14 16:53:40 +0530

    adding tests for specsCache and conceptsCache Initialization

--- api/infoGatherer/specDetails_test.go
+++ api/infoGatherer/specDetails_test.go
@@ -14,6 +14,7 @@ func Test(t *testing.T) { TestingT(t) }
 var _ = Suite(&MySuite{})
 
 var concept1 []byte
+var concept2 []byte
 var spec1 []byte
 
 type MySuite struct {
@@ -36,6 +37,13 @@ func (s *MySuite) TearDownTest(c *C) {
 func (s *MySuite) buildTestData() {
 	concept1 = make([]byte, 0)
 	concept1 = append(concept1, `# foo bar
+* first step with "foo"
+* say "hello" to me
+* a "final" step
+`...)
+
+	concept2 = make([]byte, 0)
+	concept2 = append(concept2, `# bar
 * first step with "foo"
 * say "hello" to me
 * a "final" step
@@ -72,6 +80,7 @@ func (s *MySuite) TestGetParsedConcepts(c *C) {
 
 	c.Assert(len(conceptsMap), Equals, 1)
 	c.Assert(conceptsMap["foo bar"], NotNil)
+	c.Assert(specInfoGatherer.conceptDictionary, NotNil)
 }
 
 func (s *MySuite) TestGetParsedStepValues(c *C) {
@@ -84,4 +93,27 @@ func (s *MySuite) TestGetParsedStepValues(c *C) {
 	c.Assert(stepValues[0].StepValue, Equals, "Say {} to {}")
 	c.Assert(stepValues[1].StepValue, Equals, "A context step")
 	c.Assert(stepValues[2].StepValue, Equals, "Step with a {}")
+}
+
+
+func (s *MySuite) TestInitSpecsCache(c *C) {
+	_, err := util.CreateFileIn(s.specsDir, "spec1.spec", spec1)
+	c.Assert(err, Equals, nil)
+	specInfoGatherer := new(SpecInfoGatherer)
+
+	specInfoGatherer.initSpecsCache()
+
+	c.Assert(len(specInfoGatherer.specsCache), Equals, 1)
+}
+
+func (s *MySuite) TestInitConceptsCache(c *C) {
+	_, err := util.CreateFileIn(s.specsDir, "concept1.cpt", concept1)
+	c.Assert(err, Equals, nil)
+	_, err = util.CreateFileIn(s.specsDir, "concept2.cpt", concept2)
+	c.Assert(err, Equals, nil)
+	specInfoGatherer := new(SpecInfoGatherer)
+
+	specInfoGatherer.initConceptsCache()
+
+	c.Assert(len(specInfoGatherer.conceptsCache), Equals, 2)
 }
