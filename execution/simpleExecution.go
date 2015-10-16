748a73148dca671c845fc228cb5ae13d671361a4
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-10-16 15:50:00 +0530

    adding specsFailedCount in simpleExecution

--- execution/simpleExecution.go
+++ execution/simpleExecution.go
@@ -133,6 +133,7 @@ func (exe *simpleExecution) start() *result.SuiteResult {
 	exe.suiteResult.ProjectName = filepath.Base(config.ProjectRoot)
 	exe.suiteResult.Environment = env.CurrentEnv
 	exe.suiteResult.Tags = ExecuteTags
+	exe.suiteResult.SpecsSkippedCount = len(exe.errMaps.specErrs)
 	initSuiteDataStoreResult := exe.initializeSuiteDataStore()
 	if initSuiteDataStoreResult.GetFailed() {
 		exe.logger.Error("Failed to initialize suite datastore. Error: %s", initSuiteDataStoreResult.GetErrorMessage())
