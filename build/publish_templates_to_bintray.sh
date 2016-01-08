4ed769b25c5455aefa734e1b2a25c22486c7bd98
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-08 12:30:04 +0530

    Bintray: Add Template upload script
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ build/publish_templates_to_bintray.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ -z "$BINTRAY_USER" ]; then
+  echo "BINTRAY_USER is not set"
+  exit 1
+fi
+
+if [ -z "$BINTRAY_API_KEY" ]; then
+  echo "BINTRAY_API_KEY is not set"
+  exit 1
+fi
+
+if [-z "$PACKAGE"]; then
+    PACKAGE="Templates"
+fi
+
+if [ -z "$BINTRAY_PACKAGE" ]; then
+    BINTRAY_PACKAGE="gauge-templates"
+fi
+
+for i in `ls *.zip`; do
+  LANG=$(echo $i | cut -d '_' -f 1);
+  URL="https://api.bintray.com/content/gauge/$PACKAGE/$BINTRAY_PACKAGE/latest/$LANG/$i?publish=1&override=1"
+  echo "Uploading to : $URL"
+
+  RESPONSE_CODE=$(curl -T $i -u$BINTRAY_USER:$BINTRAY_API_KEY $URL -I -s -w "%{http_code}" -o /dev/null);
+  if [[ "${RESPONSE_CODE:0:2}" != "20" ]]; then
+    echo "Unable to upload, HTTP response code: $RESPONSE_CODE"
+    exit 1
+  fi
+  echo "HTTP response code: $RESPONSE_CODE"
+done;
