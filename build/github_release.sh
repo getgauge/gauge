f1ea7d51b78e068be51cd3795acbd246ee99aa0a
apoorvam <apoorva_rarefriend@yahoo.com>
2016-01-13 11:31:54 +0530

    adding github_release script to release packages in github

---
+++ build/github_release.sh
@@ -0,0 +1,17 @@
+repoName=$1
+go get -v -u github.com/aktau/github-release
+
+cd deploy
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set"
+  exit 1
+fi
+
+version=$(ls $repoName-*-darwin.x86.zip | sed "s/^$repoName-\([^;]*\)-darwin.x86.zip/\1/")
+
+$GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "$repoName update" -n "$repoName $version"
+
+for i in `ls *.zip`; do
+    $GOPATH/bin/github-release -v upload -u getgauge -r $repoName -t "v$version" -n $i -f $i
+done
