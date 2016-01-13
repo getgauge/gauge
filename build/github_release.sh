10f874772925ce114146645de2278aa0354f468f
apoorvam <apoorva_rarefriend@yahoo.com>
2016-01-13 11:36:11 +0530

    adding description for github release

--- build/github_release.sh
+++ build/github_release.sh
@@ -10,7 +10,7 @@ fi
 
 version=$(ls $repoName-*-darwin.x86.zip | sed "s/^$repoName-\([^;]*\)-darwin.x86.zip/\1/")
 
-$GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "$repoName update" -n "$repoName $version"
+$GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "## New Features: ## Enhancements: ## Bug Fixes:" -n "$repoName $version"
 
 for i in `ls *.zip`; do
     $GOPATH/bin/github-release -v upload -u getgauge -r $repoName -t "v$version" -n $i -f $i
