e40e837feb10ea3f13712df0ef98932b978a8cb7
apoorvam <apoorva_rarefriend@yahoo.com>
2016-01-15 11:05:18 +0530

    uploading all the files in wd to github release draft

--- build/github_release.sh
+++ build/github_release.sh
@@ -5,10 +5,10 @@ if [ -z "$GITHUB_TOKEN" ]; then
   exit 1
 fi
 
-version=$(ls $repoName-*-darwin.x86.zip | sed "s/^$repoName-\([^;]*\)-darwin.x86.zip/\1/")
+version=$(ls $repoName-*-linux.x86.zip | sed "s/^$repoName-\([^;]*\)-linux.x86.zip/\1/")
 
 $GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "## New Features: ## Enhancements: ## Bug Fixes:" -n "$repoName $version"
 
-for i in `ls *.zip`; do
+for i in `ls`; do
     $GOPATH/bin/github-release -v upload -u getgauge -r $repoName -t "v$version" -n $i -f $i
 done
