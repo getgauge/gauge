c60e3319125280f556596e908e492b16d93164a0
Jean Jordaan <jean.jordaan@gmail.com>
2016-01-17 18:21:58 +0700

    Tweak install.sh to pass arguments to build/install/install.sh
    Also hack the version number

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
 os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
 os_arch=$(uname -m)
-latest_release='0.1.6'
+latest_release='0.3.0'
 file_name=gauge-$latest_release'-'$os_name.$os_arch
 base_url='https://github.com/getgauge/gauge/releases/download/v'$latest_release'/'$file_name
 
@@ -9,7 +9,7 @@ install_linux_image() {
 	wget $base_url'.zip'
 	unzip $file_name'.zip' -d $file_name
 	cd $file_name
-	./install.sh
+	./install.sh $@
 	cd ..
 	rm -rf $file_name $file_name'.zip'
 }
@@ -26,9 +26,9 @@ install_darwin_image() {
 }
 
 if [ $os_name == 'darwin' ]; then
-	install_darwin_image
+	install_darwin_image $@
 elif [ $os_name == 'linux' ]; then
-	install_linux_image
+	install_linux_image $@
 fi
 
 for arg in "$@"; do
