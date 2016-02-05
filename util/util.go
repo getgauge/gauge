6005ba2d12f003a29a1e11e09c99ea1e5d038b13
mahendrakariya <mahendrakariya@gmail.com>
2016-02-05 13:08:19 +0530

    adding IsProcessRunning utility func

--- util/util.go
+++ util/util.go
@@ -1,17 +1,38 @@
+// Copyright 2015 ThoughtWorks, Inc.
+
+// This file is part of Gauge.
+
+// Gauge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Gauge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
 package util
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"syscall"
 
 	"github.com/getgauge/common"
 	"github.com/getgauge/gauge/logger"
 )
 
+// NumberOfCores returns the number of CPU cores on the system
 func NumberOfCores() int {
 	return runtime.NumCPU()
 }
 
+// IsWindows returns if Gauge is running on Windows
 func IsWindows() bool {
 	return runtime.GOOS == "windows"
 }
@@ -33,4 +54,13 @@ func DownloadAndUnzip(downloadLink string, tempDir string) (string, error) {
 	logger.Debug("Unzipped to => %s\n", unzippedPluginDir)
 
 	return unzippedPluginDir, nil
+}
+
+// IsProcessRunning checks if the process with the given process id is still running
+func IsProcessRunning(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+	return process.Signal(syscall.Signal(0)) == nil
 }
