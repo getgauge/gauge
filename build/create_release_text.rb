81e8b3121631d4310293da89a48285f0ebe98b30
sriv <srikanth.ddit@gmail.com>
2016-03-31 16:33:36 +0530

    automate changelog for release

---
+++ build/create_release_text.rb
@@ -0,0 +1,54 @@
+# Copyright 2015 ThoughtWorks, Inc.
+
+# This file is part of Gauge.
+
+# Gauge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Gauge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+if ARGV.length < 1
+  puts 'Usage: ruby create_release_text.rb <repo name>
+Example: ruby create_release_text.rb gauge.
+'
+  exit 1
+end
+
+repo_name = ARGV[0]
+uri = URI("https://api.github.com/repos/getgauge/#{repo_name}/releases/latest")
+timestamp = JSON.parse(Net::HTTP.get(uri))['published_at']
+
+uri = URI("https://api.github.com/search/issues?q=repo:getgauge/#{repo_name}+closed:>#{timestamp}")
+response = Net::HTTP.get(uri)
+
+data = JSON.parse(response)
+
+categories = {"new feature" => [], "enhancement" => [], "bug" => [], "misc" => []}
+headers = {"new feature" => "New Features", "enhancement" => "Enhancements", "bug" => "Bug Fixes", "misc" => "Miscellaneous"}
+
+data['items'].each do |i|
+    issue_text = "- ##{i['number']} - #{i['title']}"
+    issue_labels = i['labels'].map {|x| x['name']}
+    if (issue_labels & ["moved", "duplicate"]).empty?
+        label = issue_labels & categories.keys
+        categories[label[0] || "misc"] << issue_text
+    end
+end
+
+categories.each_key do |k|
+    puts "## #{headers[k]}\n\n"
+    categories[k].each {|v| puts v}
+    puts "\n" 
+end
--- build/github_release.sh
+++ build/github_release.sh
@@ -1,13 +1,37 @@
-go get -v -u github.com/aktau/github-release
+# Copyright 2015 ThoughtWorks, Inc.
+
+# This file is part of Gauge.
+
+# Gauge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Gauge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ -z "$repoName" ]; then
+  echo "repoName is not set"
+  exit 1
+fi
 
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "GITHUB_TOKEN is not set"
   exit 1
 fi
 
+go get -v -u github.com/aktau/github-release
+
 version=$(ls $repoName-*-linux.x86.zip | sed "s/^$repoName-\([^;]*\)-linux.x86.zip/\1/")
 
-$GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "## New Features: ## Enhancements: ## Bug Fixes:" -n "$repoName $version"
+release_description=$(ruby create_release_text.rb $repoName)
+
+$GOPATH/bin/github-release release -u getgauge -r $repoName --draft -t "v$version" -d "$release_description" -n "$repoName $version"
 
 for i in `ls`; do
     $GOPATH/bin/github-release -v upload -u getgauge -r $repoName -t "v$version" -n $i -f $i
