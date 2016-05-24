4a0debde5446b13f369cb426e5da29a811550b65
apoorvam <apoorva_rarefriend@yahoo.com>
2016-05-24 11:08:48 +0530

    removing unit test in run_failed, should be added as func test #105

--- execution/run_failed/runFailed_test.go
+++ execution/run_failed/runFailed_test.go
@@ -25,10 +25,8 @@ import (
 
 	"github.com/getgauge/common"
 	"github.com/getgauge/gauge/config"
-	"github.com/getgauge/gauge/execution/event"
 	"github.com/getgauge/gauge/execution/result"
 	"github.com/getgauge/gauge/gauge_messages"
-	"github.com/getgauge/gauge/util"
 
 	. "gopkg.in/check.v1"
 )
@@ -37,19 +35,7 @@ func Test(t *testing.T) { TestingT(t) }
 
 type MySuite struct{}
 
-type MySuite1 struct{}
-
 var _ = Suite(&MySuite{})
-var _ = Suite(&MySuite1{})
-
-func (s *MySuite1) SetUpTest(c *C) {
-	failedMeta = newFailedMetaData()
-}
-
-func (s *MySuite1) TearDownTest(c *C) {
-	p, _ := filepath.Abs("_testdata")
-	os.RemoveAll(filepath.Join(p, dotGauge))
-}
 
 func (s *MySuite) TestIfFailedFileIsCreated(c *C) {
 	p, _ := filepath.Abs("_testdata")
@@ -66,62 +52,6 @@ func (s *MySuite) TestIfFailedFileIsCreated(c *C) {
 
 	c.Assert(string(content), Equals, expected)
 	os.RemoveAll(filepath.Join(config.ProjectRoot, dotGauge))
-}
-
-func (s *MySuite1) TestListenToSpecFailure(c *C) {
-	p, _ := filepath.Abs("_testdata")
-	config.ProjectRoot = p
-	event.InitRegistry()
-	specRel := filepath.Join("specs", "example.spec")
-	specAbs := filepath.Join(p, specRel)
-	failedMeta.Flags["-tags"] = "tag1"
-	failedMeta.Flags["-verbose"] = "true"
-
-	ListenFailedScenarios()
-	sr := &result.SpecResult{IsFailed: true, ProtoSpec: &gauge_messages.ProtoSpec{FileName: &specAbs}, FailedScenarioIndices: []int{2}}
-	event.Notify(event.NewExecutionEvent(event.SuiteEnd, nil, &result.SuiteResult{SpecResults: []*result.SpecResult{sr}}, 0))
-
-	contents, _ := common.ReadFileContents(filepath.Join(p, dotGauge, failedFile))
-	pathInFile := specRel
-	if util.IsWindows() {
-		pathInFile = `specs\\example.spec`
-	}
-	expected := `{
-	"Flags": {
-		"-tags": "tag1",
-		"-verbose": "true"
-	},
-	"FailedScenarios": [
-		` + `"` + pathInFile + `:2"
-	]
-}`
-	c.Assert(contents, Equals, expected)
-}
-
-func (s *MySuite1) TestListenToSpecPass(c *C) {
-	p, _ := filepath.Abs("_testdata")
-	config.ProjectRoot = p
-	event.InitRegistry()
-	specRel := filepath.Join("specs", "example.spec")
-	specAbs := filepath.Join(p, specRel)
-	failedMeta.Flags["-tags"] = "tag1 & tag2"
-	failedMeta.Flags["-verbose"] = "true"
-	failedMeta.Flags["-env"] = "chrome"
-
-	ListenFailedScenarios()
-	sr := &result.SpecResult{IsFailed: false, ProtoSpec: &gauge_messages.ProtoSpec{FileName: &specAbs}}
-	event.Notify(event.NewExecutionEvent(event.SuiteEnd, nil, &result.SuiteResult{SpecResults: []*result.SpecResult{sr}}, 0))
-
-	contents, _ := common.ReadFileContents(filepath.Join(p, dotGauge, failedFile))
-	expected := `{
-	"Flags": {
-		"-env": "chrome",
-		"-tags": "tag1 \u0026 tag2",
-		"-verbose": "true"
-	},
-	"FailedScenarios": []
-}`
-	c.Assert(contents, Equals, expected)
 }
 
 func (s *MySuite) TestGetFailedMetadata(c *C) {
