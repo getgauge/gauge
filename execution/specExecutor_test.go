1ec34e4a247ca06ec4c6436dff36f0524c189455
max <max@10.99.94.194>
2016-06-03 15:33:13 -0400

    add test skip spec if invalid data range

--- execution/specExecutor_test.go
+++ execution/specExecutor_test.go
@@ -276,34 +276,29 @@ func (s *MySuite) TestToGetDataTableRow(c *C) {
 }
 
 func (s *MySuite) TestToGetDataTableRowFromInvalidInput(c *C) {
-	dataRange, err := getDataTableRowsRange("a", 7)
-	assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("a-5", 7)
-	assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("a-qwerty", 7)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("aas-helloo", 7)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("apoorva", 7)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("8-9", 7)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("12-9", 7)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("4:5", 6)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("4-5-8", 6)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("4", 3)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("0", 3)
-  assertErrorsForDataTable(c, dataRange, err)
-	dataRange, err = getDataTableRowsRange("", 3)
-	assertErrorsForDataTable(c, dataRange, err)
-}
-
-func assertErrorsForDataTable(c *C,dataRange indexRange,err error){
-	c.Assert(dataRange.start,Equals,-1)
+	_, err := getDataTableRowsRange("a", 7)
+	c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("a-5", 7)
+	c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("a-qwerty", 7)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("aas-helloo", 7)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("apoorva", 7)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("8-9", 7)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("12-9", 7)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("4:5", 6)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("4-5-8", 6)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("4", 3)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("0", 3)
+  c.Assert(err.Error(), Equals, "Table rows range validation failed.")
+	_, err = getDataTableRowsRange("", 3)
 	c.Assert(err.Error(), Equals, "Table rows range validation failed.")
 }
 
@@ -326,6 +321,10 @@ func (s *MySuite) TestCreateSkippedSpecResult(c *C) {
 }
 
 func (s *MySuite) TestCreateSkippedSpecResultWithScenarios(c *C) {
+<<<<<<< 016ee499301141bce67355a4d76c56ee703cfab6
+=======
+
+>>>>>>> add test skip spec if invalid data range
 	se := newSpecExecutor(anySpec(), nil, nil, indexRange{start: 0, end: 0}, nil, 0)
 	se.errMap = getValidationErrorMap()
 	se.specResult = &result.SpecResult{ProtoSpec: &gauge_messages.ProtoSpec{}}
@@ -346,13 +345,21 @@ func (s *MySuite) TestCreateSkippedSpecResultWithScenarios(c *C) {
 	// c.Assert(specExecutor.errMap.ScenarioErrs[spec.Scenarios[0]][0].step.LineText, Equals, "A spec heading")
 }
 
+<<<<<<< 016ee499301141bce67355a4d76c56ee703cfab6
 func anySpec() *gauge.Specification {
+=======
+func anySpec() *gauge.Specification{
+>>>>>>> add test skip spec if invalid data range
 	specText := SpecBuilder().specHeading("A spec heading").
 		scenarioHeading("First scenario").
 		step("create user \"456\" \"foo\" and \"9900\"").
 		String()
 
+<<<<<<< 016ee499301141bce67355a4d76c56ee703cfab6
 	spec, _ := new(parser.SpecParser).Parse(specText, gauge.NewConceptDictionary())
+=======
+	spec, _ := new(parser.SpecParser).Parse(specText, gauge.NewConceptDictionary(), "")
+>>>>>>> add test skip spec if invalid data range
 	spec.FileName = "FILE"
 	return spec
 }
@@ -363,9 +370,15 @@ func (s *MySuite) TestSpecIsSkippedIfDataRangeIsInvalid(c *C) {
 		ScenarioErrs: make(map[*gauge.Scenario][]*validation.StepValidationError),
 		StepErrs:     make(map[*gauge.Step]*validation.StepValidationError),
 	}
+<<<<<<< 016ee499301141bce67355a4d76c56ee703cfab6
 	se := newSpecExecutor(anySpec(), nil, nil, indexRange{start: -1, end: -1}, errMap, 0)
 
 	result := se.execute()
+=======
+	se := newSpecExecutor(anySpec(), nil, nil, indexRange{start: -1, end: -1},errMap , 0)
+
+	result:= se.execute()
+>>>>>>> add test skip spec if invalid data range
 
 	c.Assert(result.Skipped, Equals, true)
 }
