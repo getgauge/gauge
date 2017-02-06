6eeaf0698b8e64a0442eca54d7fb5b22a5be1a7b
BugDiver <vinaysh@thoughtworks.com>
2017-02-06 12:20:26 +0530

    Bumped version to 0.8.0

--- version/version.go
+++ version/version.go
@@ -25,7 +25,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{0, 7, 1}
+var CurrentGaugeVersion = &Version{0, 8, 0}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
