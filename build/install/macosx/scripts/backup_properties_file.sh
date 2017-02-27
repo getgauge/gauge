dea9bdcfbd6977c08b70bbb043de38bef4723a18
BugDiver <vinaysh@thoughtworks.com>
2017-02-27 14:26:34 +0530

    Updating post install script to copy config files in .gauge/config

--- build/install/macosx/scripts/backup_properties_file.sh
+++ build/install/macosx/scripts/backup_properties_file.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-prefix=/usr/local
-gaugePropertiesFile=$prefix/share/gauge/gauge.properties
-timestamp_file=$prefix/share/gauge/timestamp.txt
+config=$HOME/config
+gaugePropertiesFile=$config/gauge.properties
+timestamp_file=$config/timestamp.txt
 
 if [ -f $timestamp_file ] ; then
     currentTimeStamp=`stat -f "%m" $gaugePropertiesFile`
     oldTimeStamp=`cat $timestamp_file`
     if [ $currentTimeStamp != $oldTimeStamp ] ; then
-        backupFile=$prefix/share/gauge/gauge.properties.bak
-        echo "If you have Gauge installed already and there are any manual changes in gauge.properties file, a backup of it has been taken at GAUGE_INSTALL_LOCATION\share\gauge\gauge.properties.bak. You can restore these configurations later."
+        backupFile=$config/gauge.properties.bak
+        echo "If you have Gauge installed already and there are any manual changes in gauge.properties file, a backup of it has been taken at GAUGE_INSTALL_LOCATION\config\gauge.properties.bak or HOME\.gauge\config\gauge.properties.bak. You can restore these configurations later."
         rm -rf $backupFile
         cat $gaugePropertiesFile > $backupFile
     fi
--- build/install/macosx/scripts/timestamp_and_install_plugins.sh
+++ build/install/macosx/scripts/timestamp_and_install_plugins.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+config=$HOME/.gauge/config
+
+# create config dir if doesn't exist
+if [[ ! -d $config ]]; then
+    mkdir -p $config
+fi
+
+# Copy config file from /usr/local/share to HOME/.gauge/config
+cp -r /usr/local/share/gauge/* $config
+
 # save timestamp of gauge.properties file
-prefix=/usr/local
-gaugePropertiesFile=$prefix/share/gauge/gauge.properties
-timestamp_file=$prefix/share/gauge/timestamp.txt
+config=$HOME/.gauge/config
+gaugePropertiesFile=$config/gauge.properties
+timestamp_file=$config/timestamp.txt
 
 rm $prefix/share/gauge/timestamp.txt
-stat -f "%m" $gaugePropertiesFile > $prefix/share/gauge/timestamp.txt
+stat -f "%m" $gaugePropertiesFile > $config/timestamp.txt
 
 # install default gauge plugins
 sudo -u $USER /usr/local/bin/gauge --install html-report
