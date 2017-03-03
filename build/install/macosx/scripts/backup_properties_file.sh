a1843e2a0015c9d675661973132e3b5429817e45
Apoorva G A <apoorvag@thoughtworks.com>
2017-03-03 12:10:20 +0530

    creating config with sudo access

--- build/install/macosx/scripts/backup_properties_file.sh
+++ build/install/macosx/scripts/backup_properties_file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-config=$HOME/config
+config=$HOME/gauge/config
 gauge_properties_file=$config/gauge.properties
 timestamp_file=$config/timestamp.txt
 
@@ -9,7 +9,7 @@ if [ -f $timestamp_file ] ; then
     old_time_stamp=`cat $timestamp_file`
     if [ $current_time_stamp != $old_time_stamp ] ; then
         backup_file=$config/gauge.properties.bak
-        echo "If you have Gauge installed already and there are any manual changes in gauge.properties file, a backup of it has been taken at GAUGE_INSTALL_LOCATION\config\gauge.properties.bak or HOME\.gauge\config\gauge.properties.bak. You can restore these configurations later."
+        echo "If you have Gauge installed already and there are any manual changes in gauge.properties file, a backup of it has been taken at HOME\.gauge\config\gauge.properties.bak. You can restore these configurations later."
         rm -rf $backup_file
         cat $gauge_properties_file > $backup_file
     fi
