c706c689492f539e50519df750e473d65da8d39f
sriv <srikanth.ddit@gmail.com>
2017-03-15 17:47:27 +0530

    Revert "remove logic to add git SHA/branch for local builds, #584"
    This reverts commit 209e35192c2d06d4ce8d61901314946db8e5d585.

--- build/make.go
+++ build/make.go
@@ -31,6 +31,7 @@ import (
 
 	"github.com/getgauge/common"
 	"github.com/getgauge/gauge/version"
+	"github.com/mattn/go-isatty"
 )
 
 const (
@@ -215,15 +216,9 @@ func main() {
 	flag.Parse()
 	if *nightly {
 		buildMetadata = fmt.Sprintf("nightly-%s", time.Now().Format(nightlyDatelayout))
-	} 
-	// disabled this temporarily.
-	// dependency on external package breaks vendoring, since make.go is in a different package, i.e. not in gauge
-	// os.Stdin.Stat is the way to go, but it doesnt work on windows. Fix tentatively in go1.9
-	// ref: https://github.com/golang/go/issues/14853
-
-	// else if isatty.IsTerminal(os.Stdout.Fd()) {
-	// 	buildMetadata = fmt.Sprintf("%s%s", buildMetadata, revParseHead())
-	// }
+	} else if isatty.IsTerminal(os.Stdout.Fd()) {
+		buildMetadata = fmt.Sprintf("%s%s", buildMetadata, revParseHead())
+	}
 	fmt.Println("Build: " + buildMetadata)
 	if *test {
 		runTests(*coverage)
