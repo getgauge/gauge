02073263b08fe55b723ebb1309dae1b2e9f61b7a
kashishm <kashishmunjal64@gmail.com>
2017-07-03 15:02:20 +0530

    Refactoring config command #681

--- cmd/config.go
+++ cmd/config.go
@@ -1,42 +1,46 @@
+// Copyright 2015 ThoughtWorks, Inc.
+
+// This file is part of Gauge.
+
+// Gauge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Gauge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
 package cmd
 
 import (
+	"os"
+
 	"github.com/getgauge/gauge/config"
 	"github.com/getgauge/gauge/logger"
 	"github.com/spf13/cobra"
 )
 
 var (
-	propertyCmd = &cobra.Command{
+	configCmd = &cobra.Command{
 		Use:     "config [flags] [args]",
-		Short:   "Manage global config properties",
-		Long:    "Manage global config properties.",
+		Short:   "Change global configurations",
+		Long:    "Change global configurations.",
 		Example: `  gauge config check_updates false`,
 		Run: func(cmd *cobra.Command, args []string) {
 			setGlobalFlags()
 			if list || machineReadable {
-				var f config.Formatter
-				f = config.TextFormatter{}
-				if machineReadable {
-					f = &config.JsonFormatter{}
-				}
-				s, err := config.MergedProperties().Format(f)
-				if err != nil {
-					logger.Fatalf(err.Error())
-				}
-				logger.Info(s)
-				return
+				exit(config.List(machineReadable))
 			}
 			if len(args) == 0 {
 				logger.Fatalf("Error: Config command needs argument(s).\n%s", cmd.UsageString())
 			}
 			if len(args) == 1 {
-				v, err := config.GetProperty(args[0])
-				if err != nil {
-					logger.Fatalf(err.Error())
-				}
-				logger.Info(v.Value)
-				return
+				exit(config.GetProperty(args[0]))
 			}
 			if err := config.Update(args[0], args[1]); err != nil {
 				logger.Fatalf(err.Error())
@@ -47,7 +51,15 @@ var (
 )
 
 func init() {
-	GaugeCmd.AddCommand(propertyCmd)
-	propertyCmd.Flags().BoolVarP(&list, "list", "", false, "List all global properties")
-	propertyCmd.Flags().BoolVarP(&machineReadable, "machine-readable", "m", false, "Print all properties in JSON format")
+	GaugeCmd.AddCommand(configCmd)
+	configCmd.Flags().BoolVarP(&list, "list", "", false, "List all global properties")
+	configCmd.Flags().BoolVarP(&machineReadable, "machine-readable", "m", false, "Print all properties in JSON format")
+}
+
+func exit(text string, err error) {
+	if err != nil {
+		logger.Fatalf(err.Error())
+	}
+	logger.Info(text)
+	os.Exit(0)
 }
--- config/formatter.go
+++ config/formatter.go
@@ -1,3 +1,20 @@
+// Copyright 2015 ThoughtWorks, Inc.
+
+// This file is part of Gauge.
+
+// Gauge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Gauge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
+
 package config
 
 import (
@@ -6,22 +23,22 @@ import (
 	"strings"
 )
 
-type Formatter interface {
-	Format([]property) (string, error)
+type formatter interface {
+	format([]property) (string, error)
 }
 
-type JsonFormatter struct {
+type jsonFormatter struct {
 }
 
-func (f JsonFormatter) Format(p []property) (string, error) {
+func (f jsonFormatter) format(p []property) (string, error) {
 	bytes, err := json.MarshalIndent(p, "", "\t")
 	return string(bytes), err
 }
 
-type TextFormatter struct {
+type textFormatter struct {
 }
 
-func (f TextFormatter) Format(p []property) (string, error) {
+func (f textFormatter) format(p []property) (string, error) {
 	format := "%-30s\t%-35s"
 	var s []string
 	max := 0
