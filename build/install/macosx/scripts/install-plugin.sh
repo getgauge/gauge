d865cbe15b6bfd11575c1082928169db4d91daf4
BugDiver <vinaysh@thoughtworks.com>
2017-08-04 11:04:12 +0530

    renamed c# to CSharp in mac installer

--- build/install/macosx/scripts/install-plugin.sh
+++ build/install/macosx/scripts/install-plugin.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-plugin_name=$INSTALL_PKG_SESSION_ID
 
-if [ "$plugin_name" == "c#" ]
-then
-    plugin_name="csharp"
-fi
-
-sudo -u $USER /usr/local/bin/gauge install $plugin_name
+sudo -u $USER /usr/local/bin/gauge install $INSTALL_PKG_SESSION_ID
