699c64835be114be380ba5bef44876ffa63f2499
Apoorva G A <apoorvag@thoughtworks.com>
2017-09-18 11:32:42 +0530

    Refactoring #806

--- parser/specparser.go
+++ parser/specparser.go
@@ -805,9 +805,8 @@ type ParseError struct {
 func (se ParseError) Error() string {
 	if se.LineNo == 0 && se.FileName == "" {
 		return fmt.Sprintf("%s", se.Message)
-	} else {
-		return fmt.Sprintf("%s:%d %s => '%s'", se.FileName, se.LineNo, se.Message, se.LineText)
 	}
+	return fmt.Sprintf("%s:%d %s => '%s'", se.FileName, se.LineNo, se.Message, se.LineText)
 }
 
 func (token *Token) String() string {
