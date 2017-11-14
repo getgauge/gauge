80d52c1dc825083edf7a47d986233e380b3a397e
BugDiver <vinaysh@thoughtworks.com>
2017-11-14 18:28:03 +0530

    Added custom reponse in lsp to send all scenarios info in agiven file

--- api/lang/server.go
+++ api/lang/server.go
@@ -149,6 +149,8 @@ func (h *LangHandler) Handle(ctx context.Context, conn jsonrpc2.JSONRPC2, req *j
 		return nil, errors.New("Unknown request")
 	case "textDocument/codeAction":
 		return getCodeActions(req)
+	case "gauge/scenarios":
+		return getScenarios(req)
 	default:
 		return nil, errors.New("Unknown request")
 	}
