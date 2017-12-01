4400f4f7fcffcf164712ee4f51a60990a88bcb43
nehashri <nehashrl@thoughtworks.com>
2017-12-01 14:49:53 +0530

    Adding messages from scenario hooks

--- execution/scenarioExecutor.go
+++ execution/scenarioExecutor.go
@@ -121,6 +121,7 @@ func (e *scenarioExecutor) notifyBeforeScenarioHook(scenarioResult *result.Scena
 		ScenarioExecutionStartingRequest: &gauge_messages.ScenarioExecutionStartingRequest{CurrentExecutionInfo: e.currentExecutionInfo}}
 	e.pluginHandler.NotifyPlugins(message)
 	res := executeHook(message, scenarioResult, e.runner)
+	scenarioResult.ProtoScenario.Message = append(scenarioResult.ProtoScenario.Message, res.Message...)
 	if res.GetFailed() {
 		setScenarioFailure(e.currentExecutionInfo)
 		handleHookFailure(scenarioResult, res, result.AddPreHook)
@@ -131,6 +132,7 @@ func (e *scenarioExecutor) notifyAfterScenarioHook(scenarioResult *result.Scenar
 	message := &gauge_messages.Message{MessageType: gauge_messages.Message_ScenarioExecutionEnding,
 		ScenarioExecutionEndingRequest: &gauge_messages.ScenarioExecutionEndingRequest{CurrentExecutionInfo: e.currentExecutionInfo}}
 	res := executeHook(message, scenarioResult, e.runner)
+	scenarioResult.ProtoScenario.Message = append(scenarioResult.ProtoScenario.Message, res.Message...)
 	if res.GetFailed() {
 		setScenarioFailure(e.currentExecutionInfo)
 		handleHookFailure(scenarioResult, res, result.AddPostHook)
