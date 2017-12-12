f4938ba91dbd3902d8798bcba897e0fa38bef641
Saakallya Biswas <riju91@gmail.com>
2017-12-12 12:10:44 +0530

    Breaking Tests into smaller chunks for handle repeat #862

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -5,20 +5,42 @@ import (
 	"path/filepath"
 	"testing"
 
+	"reflect"
+
 	"github.com/getgauge/gauge/config"
 	"github.com/spf13/cobra"
-	. "gopkg.in/check.v1"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+var path = ""
 
-type MySuite struct{}
-
-var _ = Suite(&MySuite{})
-
-func (s *MySuite) TestHandleRepeatCommand(c *C) {
-	path, _ := filepath.Abs("_testData")
+func before() {
+	path, _ = filepath.Abs("_testData")
 	config.ProjectRoot = path
+}
+
+func after() {
+	os.RemoveAll(path)
+}
+
+func TestMain(m *testing.M) {
+	before()
+	runTests := m.Run()
+	after()
+	os.Exit(runTests)
+}
+
+func TestHandleRepeatCommandForWriteCommandFlow(t *testing.T) {
+	args := []string{"gauge", "run", "specs"}
+	cmd := &cobra.Command{}
+
+	handleRepeatCommand(cmd, args)
+	commandWritten := readPrevCmd()
+	if !reflect.DeepEqual(commandWritten.Command, args) {
+		t.Errorf("Expected %v  Got %v", args, commandWritten.Command)
+	}
+}
+
+func TestHandleRepeatCommandForRepeatCommandFlow(t *testing.T) {
 	args := []string{"gauge", "run", "specs"}
 	cmd := &cobra.Command{}
 
@@ -28,21 +50,29 @@ func (s *MySuite) TestHandleRepeatCommand(c *C) {
 	}
 
 	handleRepeatCommand(cmd, args)
-	commandWritten := readPrevCmd()
-	c.Assert(commandWritten.Command, DeepEquals, args)
 
 	repeat = true
 	handleRepeatCommand(cmd, args)
-	c.Assert(execFlowFlag, Equals, true)
-	commandWritten = readPrevCmd()
-	c.Assert(commandWritten.Command, DeepEquals, args)
+	if !execFlowFlag {
+		t.Errorf("Expected %v  Got %v", true, execFlowFlag)
+	}
+	commandWritten := readPrevCmd()
+	if !reflect.DeepEqual(commandWritten.Command, args) {
+		t.Errorf("Expected %v  Got %v", args, commandWritten.Command)
+	}
+}
 
-	repeat = false
+func TestHandleRepeatCommandForFailedCommandFlow(t *testing.T) {
+	args := []string{"gauge", "run", "specs"}
+	cmd := &cobra.Command{}
+
+	handleRepeatCommand(cmd, args)
+
 	prevFailed = true
 	args2 := []string{"something", "else"}
 	handleRepeatCommand(cmd, args2)
-	commandWritten = readPrevCmd()
-	c.Assert(commandWritten.Command, DeepEquals, args)
-
-	os.RemoveAll(path)
+	commandWritten := readPrevCmd()
+	if !reflect.DeepEqual(commandWritten.Command, args) {
+		t.Errorf("Expected %v  Got %v", args, commandWritten.Command)
+	}
 }
