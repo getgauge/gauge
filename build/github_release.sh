d6da48dd0e5de14c08714fee0fb6637139feda5f
BugDiver <vinaysh@thoughtworks.com>
2017-12-18 16:22:00 +0530

    update create_release_text script

--- build/github_release.sh
+++ build/github_release.sh
@@ -42,13 +42,13 @@ echo "------------------------------"
 echo "Releasing $repoName v$version"
 echo "------------------------------"
 
-release_description=$(ruby -e "$(curl -sSfL https://github.com/getgauge/gauge/raw/master/build/create_release_text.rb)" $repoName)
+release_description=$(ruby -e "$(curl -sSfL https://github.com/getgauge/gauge/raw/master/build/create_release_text.rb)" $repoName $githubUser)
 
 $GOPATH/bin/github-release release -u $githubUser -r $repoName --draft -t "v$version" -d "$release_description" -n "$repoName $version"
 
 for i in `ls`; do
     $GOPATH/bin/github-release -v upload -u $githubUser -r $repoName -t "v$version" -n $i -f $i
-    if [$? -ne 0];then
+    if [ $? -ne 0 ];then
         exit 1
     fi
 done
