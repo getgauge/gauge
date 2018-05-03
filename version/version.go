289145822ae7b3f154d45779fcb80180a8ef126a
nehashri <nehashrl@thoughtworks.com>
2018-05-03 16:51:40 +0530

    bumping version 0.9.9

--- version/version.go
+++ version/version.go
@@ -25,7 +25,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{0, 9, 8}
+var CurrentGaugeVersion = &Version{0, 9, 9}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
