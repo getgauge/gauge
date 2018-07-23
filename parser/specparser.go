538d03227004534ac2238e67c3a3f00f8b583dde
Subham Singh <s.chauhan5505@gmail.com>
2018-07-23 13:37:55 +0530

    Remove unnecessary conditional branch, #1102

--- parser/specparser.go
+++ parser/specparser.go
@@ -751,11 +751,9 @@ func validateTableRows(token *Token, argLookup *gauge.ArgLookup, fileName string
 			match := specialArgMatcher.FindAllStringSubmatch(tableValue, -1)
 			param := match[0][1]
 			file :=  strings.TrimSpace(strings.TrimPrefix(param,"file:"))
+			tableValues = append(tableValues, gauge.TableCell{Value: param, CellType: gauge.SpecialString})
 			if _, err := util.GetFileContents(file); err !=nil {
-				tableValues = append(tableValues, gauge.TableCell{Value: param, CellType: gauge.SpecialString})
 				error = append(error, ParseError{FileName: fileName, LineNo: token.LineNo, Message: fmt.Sprintf("Dynamic param <%s> could not be resolved, Missing file: %s", param,file),LineText:token.LineText})
-			} else {
-				tableValues = append(tableValues, gauge.TableCell{Value: param, CellType: gauge.SpecialString})
 			}
 		} else if dynamicArgMatcher.MatchString(tableValue) {
 			match := dynamicArgMatcher.FindAllStringSubmatch(tableValue, -1)
