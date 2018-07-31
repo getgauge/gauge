d757c4eb925f093cb41c7300ff9104a0de5c94a8
sriv <srikanth.ddit@gmail.com>
2018-07-31 07:33:56 +0530

    fix machine readable for plugin output, #1126

--- reporter/jsonConsole.go
+++ reporter/jsonConsole.go
@@ -227,7 +227,7 @@ func (c *jsonConsole) Errorf(err string, args ...interface{}) {
 func (c *jsonConsole) Write(b []byte) (int, error) {
 	c.Lock()
 	defer c.Unlock()
-	fmt.Fprint(c.writer, string(b)+newline)
+	fmt.Fprintf(c.writer, "{\"type\": \"out\", \"message\": \"%s\"}", b)
 	return len(b), nil
 }
 
