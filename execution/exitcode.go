3750b265b943d47cba1116798adabb5a71f0799c
Srikanth <srikanth.ddit@gmail.com>
2018-08-21 10:17:58 +0530

    rename: no-exit-code to fail-safe (#1156)

---
+++ execution/exitcode.go
@@ -0,0 +1,12 @@
+package execution
+
+const (
+	// Success indicated successful operation
+	Success = 0
+	// ExecutionFailed indicates gauge's execution failed
+	ExecutionFailed = 1
+	// ParseFailed indicates one or more parse errors
+	ParseFailed = 2
+	// ValidationFailed indicates one or more validation errors
+	ValidationFailed = 3
+)
