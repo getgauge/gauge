cdff8ac26afac0f30e694c5a20158fc377bb9a62
Ankoncha <44019225+luciferankon@users.noreply.github.com>
2019-07-03 15:04:11 +0530

    Writing to log files for plugin fatal error #1426

--- logger/logWriter.go
+++ logger/logWriter.go
@@ -72,7 +72,9 @@ func (w Writer) Write(p []byte) (int, error) {
 			GetLogger(w.loggerID).Warning(m.Message)
 		}
 	case "fatal":
-		write(w.ShouldWriteToStdout, m.Message)
+		if initialized {
+			GetLogger(w.loggerID).Warning(m.Message)
+		}
 		fatalErrors = append(fatalErrors, fmt.Sprintf("[%s]\n\t%s", w.loggerID, m.Message))
 		//TODO: Aggregate the fatal erros from the plugins and print it at the end of execution
 		// Or print them when Gauge's fataf is used.
