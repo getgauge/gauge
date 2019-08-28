477b4ae2cb6045b682c141cbe5ed3693f4fdc832
BugDiver <vinaysh@thoughtworks.com>
2019-08-28 16:05:09 +0530

    Update release script to use hub instead githib-release. #1468

--- build/github_release.sh
+++ build/github_release.sh
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Gauge.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+
 if [ -z "$githubUser" ]; then
   echo "userName is not set. using getgauge as default."
   githubUser="getgauge"
@@ -35,39 +37,66 @@ if [ -z "$GITHUB_TOKEN" ]; then
   exit 1
 fi
 
-go get -v -u github.com/aktau/github-release
 if [ -z "$version" ]; then
   version=$(ls $artifactName* | head -1 | sed "s/\.[^\.]*$//" | sed "s/$artifactName-//" | sed "s/-[a-z]*\.[a-z0-9_]*$//");
 fi
 
+go get github.com/github/hub
+hub --version
+
+dest="/tmp"
+
+function clone_and_change_repo() {
+    rm -rf "$dest/$repoName"
+    echo "-----------------------------------------"
+    echo "Cloning $githubUser/$repoName to $dest/$repoName"
+    echo "-----------------------------------------"
+    hub clone "$githubUser/$repoName" "$dest/$repoName" --depth 1
+    cd "$dest/$repoName"
+}
+
 function draft_a_release() {
-  echo "------------------------------"
-  echo "Releasing $repoName v$version"
-  echo "------------------------------"
-
-  release_description=$(ruby -e "$(curl -sSfL https://github.com/getgauge/gauge/raw/master/build/create_release_text.rb)" $repoName $githubUser)
-
-  $GOPATH/bin/github-release release -u $githubUser -r $repoName --draft -t "v$version" -d "$release_description" -n "$repoName $version"
-
-  if [ -z "$uploadArtifact" -o "$uploadArtifact" == "yes" ]; then
-    echo "Start uploading artifacts ..."
+    artifacts=()
+    dir=`pwd`
     for i in `ls`; do
-        $GOPATH/bin/github-release -v upload -u $githubUser -r $repoName -t "v$version" -n $i -f $i
-        if [ $? -ne 0 ];then
-            exit 1
-        fi
+        artifacts+="$dir/$i "
     done
-  else
-    echo "Avoiding uploading artifact as uploadArtifact is not set to yes."
-  fi
+
+    clone_and_change_repo
+
+    echo "-----------------------------------------"
+    echo "Drafting release v$version for $repoName "
+    echo "-----------------------------------------"
+
+    echo "$repoName v$version\n\n" > desc.txt
+
+    release_description=$(ruby -e "$(curl -sSfL https://github.com/getgauge/gauge/raw/master/build/create_release_text.rb)" $repoName $githubUser)
+    echo "$release_description" >> desc.txt
+
+    hub release create -d -F ./desc.txt $version
+
+    rm -rf desc.txt
+
+    if [ -z "$uploadArtifact" -o "$uploadArtifact" == "yes" ]; then
+        echo "Start uploading ..."
+        for i in `ls $artifacts`; do
+            hub release edit -m "" -a $i $version
+            if [ $? -ne 0 ];then
+                exit 1
+            fi
+        done
+    else
+        echo "Avoiding uploading artifact as uploadArtifact is not set to yes."
+    fi
+    rm -rf "$dest/$repoName"
 }
 
 function publish_a_release() {
-  echo "------------------------------"
-  echo "Publishing $repoName v$version"
-  echo "------------------------------"
-
-  $GOPATH/bin/github-release edit -u $githubUser -r $repoName -t "v$version" -n "$repoName $version"
+    clone_and_change_repo
+    echo "-------------------------------------------"
+    echo "Publishing release v$version for $repoName "
+    echo "-------------------------------------------"
+    hub release edit -m "" --draft=false $version
 }
 
 if [ -z "$releaseType" -o "$releaseType" == "draft" ]; then
