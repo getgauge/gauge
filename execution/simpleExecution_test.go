e093fe28985030f32908d4deb7f12f2c888e62d6
Dharmendra Singh <dharmenn@thoughtworks.com>
2020-01-21 23:03:56 +0530

    Accept screenshot data from runners without screenshot writer. getgauge/html-report#247

--- execution/simpleExecution_test.go
+++ execution/simpleExecution_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/getgauge/gauge/execution/result"
+	"github.com/getgauge/gauge/gauge"
 
 	"github.com/getgauge/gauge/gauge_messages"
 )
@@ -87,9 +88,9 @@ func TestNotifyBeforeSuiteShouldAddsBeforeSuiteHookScreenshots(t *testing.T) {
 	r.ExecuteAndGetStatusFunc = func(m *gauge_messages.Message) *gauge_messages.ProtoExecutionResult {
 		if m.MessageType == gauge_messages.Message_ExecutionStarting {
 			return &gauge_messages.ProtoExecutionResult{
-				ScreenshotFiles:   []string{"screenshot1.png", "screenshot2.png"},
-				Failed:        false,
-				ExecutionTime: 10,
+				ScreenshotFiles: []string{"screenshot1.png", "screenshot2.png"},
+				Failed:          false,
+				ExecutionTime:   10,
 			}
 		}
 		return &gauge_messages.ProtoExecutionResult{}
@@ -141,4 +142,128 @@ func TestNotifyAfterSuiteShouldAddsAfterSuiteHookScreenshots(t *testing.T) {
 			t.Errorf("Expected `%s` screenshot, got : %s", e, afterSuiteScreenshots[i])
 		}
 	}
+}
+func TestExecuteSpecsShouldAddsBeforeSpecHookFailureScreenshotFile(t *testing.T) {
+	r := &mockRunner{}
+	h := &mockPluginHandler{NotifyPluginsfunc: func(m *gauge_messages.Message) {}, GracefullyKillPluginsfunc: func() {}}
+	r.ExecuteAndGetStatusFunc = func(m *gauge_messages.Message) *gauge_messages.ProtoExecutionResult {
+		if m.MessageType == gauge_messages.Message_SpecExecutionStarting {
+			return &gauge_messages.ProtoExecutionResult{
+				Failed:                true,
+				ExecutionTime:         10,
+				FailureScreenshotFile: "before-spec-hook-failure-screenshot.png",
+			}
+		}
+		return &gauge_messages.ProtoExecutionResult{}
+	}
+	ei := &executionInfo{runner: r, pluginHandler: h, errMaps: &gauge.BuildErrors{}}
+	simpleExecution := newSimpleExecution(ei, false)
+	specsC := createSpecCollection()
+	simpleExecution.suiteResult = result.NewSuiteResult(ExecuteTags, simpleExecution.startTime)
+	specResult := simpleExecution.executeSpecs(specsC)
+
+	actualScreenshotFile := specResult[0].ProtoSpec.PreHookFailures[0].FailureScreenshotFile
+	expectedScreenshotFile := "before-spec-hook-failure-screenshot.png"
+
+	if actualScreenshotFile != expectedScreenshotFile {
+		t.Errorf("Expected `%s` screenshot, got : %s", expectedScreenshotFile, actualScreenshotFile)
+	}
+}
+
+func TestExecuteSpecsShouldAddsAfterSpecHookFailureScreenshotFile(t *testing.T) {
+	r := &mockRunner{}
+	h := &mockPluginHandler{NotifyPluginsfunc: func(m *gauge_messages.Message) {}, GracefullyKillPluginsfunc: func() {}}
+	r.ExecuteAndGetStatusFunc = func(m *gauge_messages.Message) *gauge_messages.ProtoExecutionResult {
+		if m.MessageType == gauge_messages.Message_SpecExecutionEnding {
+			return &gauge_messages.ProtoExecutionResult{
+				Failed:                true,
+				ExecutionTime:         10,
+				FailureScreenshotFile: "after-spec-hook-failure-screenshot.png",
+			}
+		}
+		return &gauge_messages.ProtoExecutionResult{}
+	}
+	ei := &executionInfo{runner: r, pluginHandler: h, errMaps: &gauge.BuildErrors{}}
+	simpleExecution := newSimpleExecution(ei, false)
+	specsC := createSpecCollection()
+	simpleExecution.suiteResult = result.NewSuiteResult(ExecuteTags, simpleExecution.startTime)
+	specResult := simpleExecution.executeSpecs(specsC)
+
+	actualScreenshotFile := specResult[0].ProtoSpec.PostHookFailures[0].FailureScreenshotFile
+	expectedScreenshotFile := "after-spec-hook-failure-screenshot.png"
+
+	if actualScreenshotFile != expectedScreenshotFile {
+		t.Errorf("Expected `%s` screenshot, got : %s", expectedScreenshotFile, actualScreenshotFile)
+	}
+}
+
+func TestExecuteSpecsShouldAddsBeforeSpecHookFailureScreenshotBytes(t *testing.T) {
+	r := &mockRunner{}
+	h := &mockPluginHandler{NotifyPluginsfunc: func(m *gauge_messages.Message) {}, GracefullyKillPluginsfunc: func() {}}
+	r.ExecuteAndGetStatusFunc = func(m *gauge_messages.Message) *gauge_messages.ProtoExecutionResult {
+		if m.MessageType == gauge_messages.Message_SpecExecutionStarting {
+			return &gauge_messages.ProtoExecutionResult{
+				Failed:                true,
+				ExecutionTime:         10,
+				FailureScreenshot: []byte("before spec hook failure screenshot byte"),
+			}
+		}
+		return &gauge_messages.ProtoExecutionResult{}
+	}
+	ei := &executionInfo{runner: r, pluginHandler: h, errMaps: &gauge.BuildErrors{}}
+	simpleExecution := newSimpleExecution(ei, false)
+	specsC := createSpecCollection()
+	simpleExecution.suiteResult = result.NewSuiteResult(ExecuteTags, simpleExecution.startTime)
+	specResult := simpleExecution.executeSpecs(specsC)
+
+	actualScreenshotBytes := specResult[0].ProtoSpec.PreHookFailures[0].FailureScreenshot
+	expectedScreenshotBytes := "before spec hook failure screenshot byte"
+
+	if string(actualScreenshotBytes) != expectedScreenshotBytes {
+		t.Errorf("Expected `%s` screenshot, got : %s", expectedScreenshotBytes, actualScreenshotBytes)
+	}
+}
+
+func TestExecuteSpecsShouldAddsAfterSpecHookFailureScreenshotBytes(t *testing.T) {
+	r := &mockRunner{}
+	h := &mockPluginHandler{NotifyPluginsfunc: func(m *gauge_messages.Message) {}, GracefullyKillPluginsfunc: func() {}}
+	r.ExecuteAndGetStatusFunc = func(m *gauge_messages.Message) *gauge_messages.ProtoExecutionResult {
+		if m.MessageType == gauge_messages.Message_SpecExecutionEnding {
+			return &gauge_messages.ProtoExecutionResult{
+				Failed:                true,
+				ExecutionTime:         10,
+				FailureScreenshot: []byte("after spec hook failure screenshot bytes"),
+			}
+		}
+		return &gauge_messages.ProtoExecutionResult{}
+	}
+	ei := &executionInfo{runner: r, pluginHandler: h, errMaps: &gauge.BuildErrors{}}
+	simpleExecution := newSimpleExecution(ei, false)
+	specsC := createSpecCollection()
+	simpleExecution.suiteResult = result.NewSuiteResult(ExecuteTags, simpleExecution.startTime)
+	specResult := simpleExecution.executeSpecs(specsC)
+
+	actualScreenshotBytes := specResult[0].ProtoSpec.PostHookFailures[0].FailureScreenshot
+	expectedScreenshotBytes := "after spec hook failure screenshot bytes"
+
+	if string(actualScreenshotBytes) != expectedScreenshotBytes {
+		t.Errorf("Expected `%s` screenshot, got : %s", expectedScreenshotBytes, actualScreenshotBytes)
+	}
+}
+
+
+
+
+func createSpecCollection() *gauge.SpecCollection {
+	var specs []*gauge.Specification
+	specs = append(specs, &gauge.Specification{
+		FileName: "spec-1.spec",
+		Heading: &gauge.Heading{
+			Value:       "Spec heading",
+			LineNo:      1,
+			SpanEnd:     12,
+			HeadingType: 0,
+		},
+	})
+	return gauge.NewSpecCollection(specs, false)
 }
