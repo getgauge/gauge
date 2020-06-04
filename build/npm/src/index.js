6e0d83f52bad05ffeea30bfd9d979b8b78c9c0fe
Srikanth <srikanth.ddit@gmail.com>
2020-06-04 13:16:06 +0530

    correct download and extract gauge-zip, fixes #1666 (#1669)
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    Co-authored-by: Nivedha <nivedhasenthil@gmail.com>

--- build/npm/src/index.js
+++ build/npm/src/index.js
@@ -5,7 +5,6 @@
 const install = require("./install"),
     path = require("path"),
     unzip = require('unzipper'),
-    fs = require('fs'),
     request = require('superagent'),
     packageJsonPath = path.join(__dirname, "..", "package.json"),
     binPath = "./bin";
@@ -14,16 +13,15 @@ const install = require("./install"),
 var downloadAndExtract = function(version) {
     console.log(`Fetching download url for Gauge version ${version}`);
     let url = install.getBinaryUrl(version);
-    let gaugeExecutable = process.platform === "win32" ? "gauge.exe" : "gauge"
     console.log(`Downloading ${url} to ${binPath}`);
-    return unzip.Open.url(request, url).then((d) => {
-        return new Promise((resolve, reject) => {
-            d.files[0].stream()
-            .pipe(fs.createWriteStream(path.join(binPath, gaugeExecutable)))
-            .on('error',reject)
-            .on('finish',resolve)
-        });
-    });
+    return new Promise((resolve, reject) => {
+        try {
+            request.get(url).pipe(unzip.Extract({ path: path.normalize(binPath) }));
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    })
 }
 
 install.getVersion(packageJsonPath)
