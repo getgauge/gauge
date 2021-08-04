326ddc93089aaa200bdb9f7e10e08843511cf527
sriv <srikanth.ddit@gmail.com>
2021-08-04 16:18:41 +0530

    fix recursion in cmd tests
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -7,30 +7,20 @@ import (
 	"os/exec"
 	"strings"
 	"testing"
-
-	"github.com/spf13/cobra"
 )
 
 func TestGetSpecsDirFromStdin(t *testing.T) {
 	specsInput := "specs/example1.spec\nspecs/example5.spec\nspecs/example2.spec"
-	testCmd := &cobra.Command{
-		Run: func(cmd *cobra.Command, args []string) {
-			s := getSpecsDir(args)
-			if len(s) <= 0 {
-				t.Error("No specs found in stdin")
-			}
-			got := strings.Join(s, "\n")
-			if got != specsInput {
-				t.Fatalf("Expected \"%s\", got \"%s\"", specsInput, got)
-			}
-		},
-	}
-
 	if os.Getenv("TEST_EXITS") == "1" {
-		err := testCmd.Execute()
-		if err != nil {
-			t.Fatalf("unable to execute testCmd - %s", err)
+		s := getSpecsDir([]string{})
+		if len(s) <= 0 {
+			t.Error("No specs found in stdin")
 		}
+		got := strings.Join(s, "\n")
+		if got != specsInput {
+			t.Fatalf("Expected \"%s\", got \"%s\"", specsInput, got)
+		}
+		return
 	}
 	var stdout bytes.Buffer
 	cmd := exec.Command(os.Args[0], fmt.Sprintf("-test.run=%s", t.Name()))
