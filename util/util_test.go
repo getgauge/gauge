9395d57f9c0e1a1141ff7bd90a969fa712322a91
Haroon Sheikh <haroon.sheikh@sky.uk>
2023-07-20 02:53:24 +0100

    Adds gauge_concepts_dir for defining concepts directory
    Signed-off-by: Haroon Sheikh <haroon.sheikh@outlook.com>
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

--- util/util_test.go
+++ util/util_test.go
@@ -16,9 +16,3 @@ func (s *MySuite) TestSpecDirEnvVariableAllowsCommaSeparatedList(c *C) {
 	c.Assert(e, Equals, nil)
 	c.Assert(GetSpecDirs(), DeepEquals, []string{"spec1", "spec2", "spec3"})
 }
-
-func (s *MySuite) TestConceptsDirEnvVariableAllowsCommaSeparatedList(c *C) {
-	os.Clearenv()
-	os.Setenv(env.ConceptsDir, "path1")
-	c.Assert(GetConceptsPaths(), DeepEquals, []string{"path1"})
-}
