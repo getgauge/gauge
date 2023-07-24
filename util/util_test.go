4b096203c0a442f8ec101b8ecb4114376478de92
Haroon Sheikh <haroon.sheikh@sky.uk>
2023-07-24 13:38:21 +0100

    Uncomments out a unit test
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

--- util/util_test.go
+++ util/util_test.go
@@ -1,11 +1,10 @@
 package util
 
 import (
-	"os"
-
 	"github.com/getgauge/gauge/config"
 	"github.com/getgauge/gauge/env"
 	. "gopkg.in/check.v1"
+	"os"
 )
 
 func (s *MySuite) TestSpecDirEnvVariableAllowsCommaSeparatedList(c *C) {
@@ -18,8 +17,6 @@ func (s *MySuite) TestSpecDirEnvVariableAllowsCommaSeparatedList(c *C) {
 }
 
 func (s *MySuite) TestConceptsDirEnvVariableAllowsCommaSeparatedList(c *C) {
-	c.Skip("This is causing the net/httptest panic in util/httpUtils_test.go fail on windows.")
-	os.Clearenv()
 	config.ProjectRoot = "_testdata/proj1"
 
 	e := env.LoadEnv("multipleSpecs", nil)
