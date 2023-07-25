46143a638315adf19db5514ef26cb47cfac74aa1
Haroon Sheikh <haroon.sheikh@outlook.com>
2023-07-25 14:53:37 +0100

    Uncomments out a fixed unit test (#2384)

--- util/util_test.go
+++ util/util_test.go
@@ -1,15 +1,12 @@
 package util
 
 import (
-	"os"
-
 	"github.com/getgauge/gauge/config"
 	"github.com/getgauge/gauge/env"
 	. "gopkg.in/check.v1"
 )
 
 func (s *MySuite) TestSpecDirEnvVariableAllowsCommaSeparatedList(c *C) {
-	os.Clearenv()
 	config.ProjectRoot = "_testdata/proj1"
 
 	e := env.LoadEnv("multipleSpecs", nil)
@@ -18,8 +15,6 @@ func (s *MySuite) TestSpecDirEnvVariableAllowsCommaSeparatedList(c *C) {
 }
 
 func (s *MySuite) TestConceptsDirEnvVariableAllowsCommaSeparatedList(c *C) {
-	c.Skip("This is causing the net/httptest panic in util/httpUtils_test.go fail on windows.")
-	os.Clearenv()
 	config.ProjectRoot = "_testdata/proj1"
 
 	e := env.LoadEnv("multipleSpecs", nil)
