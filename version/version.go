99010bf7055f9399c32da14289a1ff74e785e0fc
Haroon Sheikh <haroon.sheikh@outlook.com>
2025-01-29 10:28:42 +0000

    Updates the gauge version

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{1, 6, 11}
+var CurrentGaugeVersion = &Version{1, 6, 12}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
