425f60ac747a2e363ff9ebd23621030ef6885466
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-02-13 18:25:50 +0800

    chore: bump version for 1.6.23 release
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{1, 6, 22}
+var CurrentGaugeVersion = &Version{1, 6, 23}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
