e03b1b70368caa088ff692c4729e56fdeb656a40
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-02-13 19:47:09 +0800

    chore: migrate to NPM trusted publishing
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{1, 6, 23}
+var CurrentGaugeVersion = &Version{1, 6, 24}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
