1120ff70dd94a0f4a47d47f9aadd9e816e10538f
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-04-10 23:26:17 +0800

    chore: remove setup-node trusted publishing hack
    Issue has possibly been resolved upstream somehow, so let's try again without hacks.
    
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 // CurrentGaugeVersion represents the current version of Gauge
-var CurrentGaugeVersion = &Version{1, 6, 29}
+var CurrentGaugeVersion = &Version{1, 6, 30}
 
 // BuildMetadata represents build information of current release (e.g, nightly build information)
 var BuildMetadata = ""
